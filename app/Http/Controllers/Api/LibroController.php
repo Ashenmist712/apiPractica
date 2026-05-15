cf445e6342a8746c737b709349ea1ca4b074e8a0
Ashenmist712 <jaelleyte12.03@gmail.com>
2026-05-14 20:16:32 -0600

    Metodos agregados en LibroController.php

--- app/Http/Controllers/Api/LibroController.php
+++ app/Http/Controllers/Api/LibroController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\libro;
 use Illuminate\Http\Request;
 
 class LibroController extends Controller
 {
-    //
+    public function index(){
+        $libros = libro::all();
+        return response()->json($libros, 200);
+        
+    }
+    public function store(Request $request){
+        $libro = libro::create($request->all());
+        return response()->json($libro, 201);
+    }
+    public function show($id){
+        $libro = libro::findOrFail($id);
+        return response()->json($libro, 200);
+    }
+    public function update(Request $request, $id){
+        $libro = libro::findOrFail($id);
+        if($libro){
+            $libro->update($request->all());
+            return response()->json($libro, 200);
+        }else{
+            return response()->json(['message' => 'Libro no encontrado'], 404);
+        }
+    }
+    public function destroy($id){
+        $libro = libro::findOrFail($id);
+        if($libro){
+            $libro->delete();
+            return response()->json(['message' => 'Libro eliminado'], 200);
+        }else{
+            return response()->json(['message' => 'Libro no encontrado'], 404);
+        }
+    }
 }
