14352c0268c8e4a9ed874b77921cc6cb0b461f93
Ashenmist712 <jaelleyte12.03@gmail.com>
2026-05-14 19:27:45 -0600

    Creacion de la migracion de la base de datos

--- database/migrations/2026_05_15_004634_create_libros_table.php
+++ database/migrations/2026_05_15_004634_create_libros_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('libros', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('genero');
+            $table->integer('anioPublicacion');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('libros');
