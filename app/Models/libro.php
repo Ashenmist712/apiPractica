d5bc12d239b105b987d782895a2972c4b296c88b
Ashenmist712 <jaelleyte12.03@gmail.com>
2026-05-14 19:49:21 -0600

    Se agrago el controlador Api

--- app/Models/libro.php
+++ app/Models/libro.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class libro extends Model
 {
-    //
+    protected $table = 'libros';
+
+    protected $fillable = [
+        'titulo',
+        'autor',
+        'genero',
+        'anioPublicacion',
+    ];
 }
