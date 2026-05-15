d060e3d1ce6b65ed07a6f60893e70155c3c74375
Ashenmist712 <jaelleyte12.03@gmail.com>
2026-05-14 19:58:12 -0600

    Se agregaron las rutas en el archivo api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\LibroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/Libros', [LibroController::class, 'index']);
+Route::post('/Libros', [LibroController::class, 'store']);
+Route::get('/Libros/{id}', [LibroController::class, 'show']);
+Route::put('/Libros/{id}', [LibroController::class, 'update']);
+Route::delete('/Libros/{id}', [LibroController::class, 'destroy']);
